a5f7d12bd9a091ddb8d301372c853fc1e7fdebc4
Annor6 <kwakuannormensah@gmail.com>
2026-02-21 09:53:56 -0800

    Implement advanced routing with nested, dynamic and protected routes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,12 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
 import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/profile/*" element={<Profile />} />
+
+        {/* Protected Nested Route */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Dynamic Route */}
         <Route path="/blog/:id" element={<BlogPost />} />
+
       </Routes>
     </Router>
   );
